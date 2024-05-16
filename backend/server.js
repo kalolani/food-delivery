7f7a1d6d8247c622a94bf3ac17994b5f149045d9
kaleab gemechu <kgemechu908@gmail.com>
2024-05-16 20:52:34 +0300

    created backend for the food delivery website

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+//config
+const app = express();
+const port = 4000;
+
+//middleware
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API workings");
+});
+
+app.listen(port, () => {
+  console.log(`server started on http://localhost:${port}`);
+});
