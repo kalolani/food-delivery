3eec3d1b75ced39ba740b57909d1d5fbb7379d8f
kaleab gemechu <kgemechu908@gmail.com>
2024-05-17 02:14:20 +0300

    setup mongodb database

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://kaleab:kalolani@cluster0.ofl3a1t.mongodb.net/food-delivery"
+    )
+    .then(() => console.log("DB Connected"));
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 //config
 const app = express();
@@ -10,10 +12,19 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
+//db connection
+connectDB();
+
+//api end point
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+
 app.get("/", (req, res) => {
-  res.send("API workings");
+  res.send("API working");
 });
 
 app.listen(port, () => {
   console.log(`server started on http://localhost:${port}`);
 });
+
+//mongodb+srv://kaleab:kalolani@cluster0.ofl3a1t.mongodb.net/?
