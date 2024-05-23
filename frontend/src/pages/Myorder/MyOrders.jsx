ca8946abf050459ef8cd524d889803902910f5a9
kaleab gemechu <kgemechu908@gmail.com>
2024-05-24 00:57:55 +0300

    created myOrder page and made it responsive

--- frontend/src/pages/Myorder/MyOrders.jsx
+++ frontend/src/pages/Myorder/MyOrders.jsx
@@ -3,20 +3,29 @@ import { useStores } from "../../contexts/storeContext";
 import "./MyOrders.css";
 import axios from "axios";
 import { assets } from "../../assets/assets";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function MyOrders() {
   const { url, token } = useStores();
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchOrders = async () => {
-    const response = await axios.post(
-      url + "/api/order/userorder",
-      {},
-      {
-        headers: { token },
-      }
-    );
-    setData(response.data.data);
+    try {
+      setIsLoading(true);
+      const response = await axios.post(
+        url + "/api/order/userorder",
+        {},
+        {
+          headers: { token },
+        }
+      );
+      setData(response.data.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -28,17 +37,22 @@ function MyOrders() {
   return (
     <div className="my-orders">
       <h2>My Orders</h2>
+      {isLoading && (
+        <div className="loadingComponent">
+          <ClipLoader color="#FF6347" loading={isLoading} size={50} />
+        </div>
+      )}
       <div className="container">
         {data.map((order, index) => {
           return (
             <div key={index} className="my-orders-order">
-              <img src={assets.parcel_icon} alt="" />
+              <img src="mosob.png" alt="" />
               <p>
                 {order.items.map((item, index) => {
                   if (index === order.items.length - 1) {
-                    return item.name + "x" + item.quantity;
+                    return item.name + "(" + item.quantity + ")";
                   } else {
-                    return item.name + "x" + item.quantity + ",";
+                    return item.name + "(" + item.quantity + ")" + ",";
                   }
                 })}
               </p>
