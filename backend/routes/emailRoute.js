53ebd7f0fdfe4b53ec42f1081289c0e89223ba3b
kaleab gemechu <kgemechu908@gmail.com>
2024-06-07 00:04:04 +0300

    added send email functionality

---
+++ backend/routes/emailRoute.js
@@ -0,0 +1,9 @@
+// backend/routes/emailRoutes.js
+import express from "express";
+import { sendEmail } from "../controllers/emailController.js";
+
+const router = express.Router();
+
+router.post("/send-email", sendEmail);
+
+export default router;
