820c1db53a64bec10c9f65689d29eaee97b7059d
kaleab gemechu <kgemechu908@gmail.com>
2024-06-11 22:08:17 +0300

    updated footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,44 +1,54 @@
-import { assets } from "../../assets/assets";
 import "./Footer.css";
 function Footer() {
   return (
     <div className="footer" id="footer">
-      <div className="footer-content">
-        <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-            tenetur ducimus facilis harum quo hic, repellendus nemo odit
-            reprehenderit iste a, id nisi ex facere doloremque modi asperiores
-            labore officia.
+      <div className="footer-logo-box">
+        <img src="dila-uni.png" alt="dilla-uni-logo" className="footer-logo" />
+      </div>
+      <div className="footer-content-container">
+        <div className="footer-nav-nav">
+          <ul className="footer-nav">
+            <li className="footer-item">
+              {" "}
+              <a href="#" className="footer-link">
+                COMPANY
+              </a>
+            </li>
+            <li className="footer-item">
+              {" "}
+              <a href="#" className="footer-link">
+                CONTACT US
+              </a>
+            </li>
+            <li className="footer-item">
+              {" "}
+              <a href="#" className="footer-link">
+                CARRERS
+              </a>
+            </li>
+            <li className="footer-item">
+              {" "}
+              <a href="#" className="footer-link">
+                PRIVACY POLICY
+              </a>
+            </li>
+            <li className="footer-item">
+              {" "}
+              <a href="#" className="footer-link">
+                TERMS
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-copyright">
+          <p className="footer-text">
+            Built by <span className="footer-name">kALEAB GEMECHU</span> for my
+            internship project. Copyright © by Kaleab Gemechu. You are 100%
+            allowed to use this webpage for both personal and commercial use,
+            but NOT to claim it as your own design.
           </p>
-          <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt=""></img>
-            <img src={assets.twitter_icon} alt=""></img>
-            <img src={assets.linkedin_icon} alt=""></img>
-          </div>
-        </div>
-        <div className="footer-content-center">
-          <h2>Company</h2>
-          <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
-          </ul>
-        </div>
-        <div className="footer-content-right">
-          <h2>GET IN TOUCH</h2>
-          <ul>
-            <li>+251916163516</li>
-            <li>kgemechu908@gmail.com</li>
-          </ul>
         </div>
       </div>
-      <hr />
-      <p className="footer-copyright">
-        Copyright @ kaleab.com - All Right Reserved
-      </p>
     </div>
   );
 }
