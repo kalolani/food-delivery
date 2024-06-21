834e6b2ba740ef3ab8d9c72da5c43e13209ab866
kal_olani <123155716+kalolani@users.noreply.github.com>
2024-06-21 14:15:59 +0300

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -60,6 +60,8 @@ import bag_icon from "./bag_icon.png";
 import logout_icon from "./logout_icon.png";
 import parcel_icon from "./parcel_icon.png";
 
+export const url="https://food-delivery-backend-yr23.onrender.com"
+
 export const assets = {
   logo,
   basket_icon,
